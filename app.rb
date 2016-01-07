6f5b9f16dd116f362f65612b5923937c82df3624
Paula Murphy <paulam849@gmail.com>
2016-01-07 18:19:49 -0500

    fixed loginpage

--- app.rb
+++ app.rb
@@ -20,3 +20,17 @@ post '/newpost' do
 	@posts = Post.create(title: params[:title], body: params[:body])
 	redirect '/'
 end	
+
+
+get "/signup" do
+	erb :signup
+end
+
+get "/login" do
+	erb :login
+end
+
+get "/logout" do
+	session.clear
+	redirect to("/logout_successful")
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106163825) do
+ActiveRecord::Schema.define(version: 20160107041831) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
   end
 
 end
